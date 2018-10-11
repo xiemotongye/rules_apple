02ebbf37fbdf0f30b55fcc4396019ff2dcbf44d7
ReinhardHuang <xiemotongye@gmail.com>
2018-10-11 18:24:47 +0800

    fix actool issue

--- tools/xctoolrunner/xctoolrunner.py
+++ tools/xctoolrunner/xctoolrunner.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+##
 """Wrapper for "xcrun" tools.
 
 This script only runs on Darwin and you must have Xcode installed.
